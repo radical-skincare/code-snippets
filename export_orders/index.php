efba89bea0d0b8024aac751f62d506dec103c447
Abdul Rehman <abdulrehmanali82@gmail.com>
2022-12-14 00:14:20 +0500

    Put everything in generate_orders_csv function and updated file name

--- export_orders/index.php
+++ export_orders/index.php
@@ -1,41 +1,45 @@
 <?php
 require('../wp-load.php');
-if (!isset($_GET['start'])) {
-  echo 'Please Enter Start Date';
-  return;
+function generate_orders_csv(){
+  if (!isset($_GET['start'])) {
+    echo 'Please Enter Start Date';
+    return;
+  }
+  if (!isset($_GET['end'])) {
+    echo 'Please Enter End Date';
+    return;
+  }
+  $initial_date = date('Y-m-d', strtotime($_GET['start']));
+  $final_date = date('Y-m-d', strtotime($_GET['end']));
+  $orders = wc_get_orders(
+    [
+      'limit' => -1,
+      'type' => 'shop_order',
+      'status' => ['wc-processing', 'wc-completed', 'wc-delivered'],
+      'date_created' => $initial_date . '...' . $final_date
+    ]
+  );
+  
+  $csv = [];
+  $csv[] = ['Date', 'Order ID', 'Billing Name', 'Billing Email', 'Billing State', 'Billing ZIP', 'Order Total'];
+  foreach ($orders as $order) {
+    $csv[] = [
+      $order->get_date_created(),
+      $order->get_id(),
+      $order->get_billing_first_name(). ' '. $order->get_billing_last_name(),
+      $order->get_billing_email(),
+      $order->get_billing_state(),
+      $order->get_billing_postcode(),
+      $order->get_total()
+    ];
+  }
+  
+  echo "File Name: <b>radical-orders_$initial_date-$final_date.csv</b> <br>";
+  $fp = fopen("radical-orders_$initial_date-$final_date.csv", 'w');
+  foreach ($csv as $fields) {
+    fputcsv($fp, $fields);
+  }
+  fclose($fp);
+  echo 'Generated';
 }
-if (!isset($_GET['end'])) {
-  echo 'Please Enter End Date';
-  return;
-}
-$initial_date = date('Y-m-d', strtotime($_GET['start']));
-$final_date = date('Y-m-d', strtotime($_GET['end']));
-$orders = wc_get_orders(
-  [
-    'limit' => -1,
-    'type' => 'shop_order',
-    'status' => ['wc-processing', 'wc-completed', 'wc-delivered'],
-    'date_created' => $initial_date . '...' . $final_date
-  ]
-);
-
-$csv = [];
-$csv[] = ['Date', 'Order ID', 'Billing Name', 'Billing Email', 'Billing State', 'Billing ZIP', 'Order Total'];
-foreach ($orders as $order) {
-  $csv[] = [
-    $order->get_date_created(),
-    $order->get_id(),
-    $order->get_billing_first_name(). ' '. $order->get_billing_last_name(),
-    $order->get_billing_email(),
-    $order->get_billing_state(),
-    $order->get_billing_postcode(),
-    $order->get_total()
-  ];
-}
- 
-$fp = fopen('orders.csv', 'w');
-foreach ($csv as $fields) {
-  fputcsv($fp, $fields);
-}
-fclose($fp);
-echo 'Generated';
+generate_orders_csv();
