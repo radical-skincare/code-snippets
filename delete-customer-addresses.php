c0b4a8b2868de80c55e0918c2622880dc6938b45
justin <jebusiness723@gmail.com>
2023-02-07 20:02:44 -0700

    Delete all customer addresses

---
+++ delete-customer-addresses.php
@@ -0,0 +1,13 @@
+<?php
+
+function delete_customer_addresses() {
+  global $wpdb;
+  $usermeta = $wpdb->prefix . "usermeta";
+  // Delete all billing addresses
+  $wpdb->query("DELETE FROM $usermeta WHERE meta_key LIKE 'billing_%'");
+  // Delete all shipping addresses
+  $wpdb->query("DELETE FROM $usermeta WHERE meta_key LIKE 'shipping_%'");
+}
+
+// Run the function when the script is loaded
+delete_customer_addresses();
